ead1009cf4e2653538f49cc422d62e89538310d5
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-11-17 11:30:21 +0900

    DAO,DTO,MemberDAO ,MemberDTO update - 01

---
+++ src/Domain/DAO.java
@@ -0,0 +1,30 @@
+package Domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DAO {
+	
+	//연결관련 정보 저장용 변수
+	String id = "SYSTEM";			//DB연결 id
+	String pw = "1234";			//DB연결 pw
+	String url = "jdbc:oracle:thin:@localhost:1521:xe"; //연결URL(DBMS마다 상이함)
+
+		//DB연결객체 관련 참조변수
+	Connection conn = null;						//DB연결객체용 참조변수
+	PreparedStatement pstmt = null;				//SQL쿼리 전송객체용 참조변수
+	ResultSet rs = null;					//쿼리결과(Select결과)수신용 참조변수
+		
+	DAO(){
+		//연결하기
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");			//DB 드라이버 로드
+			conn = DriverManager.getConnection(url,id,pw); 		//DB Connection 객체 받기
+			System.out.println("DB Connected...");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/Domain/MemberDAO.java
@@ -0,0 +1,57 @@
+package Domain;
+
+public class MemberDAO extends DAO {
+	
+	//싱글톤 패턴 코드 추가
+		private static MemberDAO instance;
+		public static MemberDAO getInstance() {
+			if(instance==null);
+				instance = new MemberDAO();
+			return instance;
+		}
+		
+	//회원가입
+	public int Insert(MemberDTO dto) {
+
+		int result=0;
+		try {
+			pstmt = conn.prepareStatement("insert into tbl_member values(?,?,?)");
+			pstmt.setString(1, dto.getMemId());
+			pstmt.setString(2, dto.getPwd());
+			pstmt.setString(3, dto.getName());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	//조회하기
+	public MemberDTO Select(String id) {
+		//pstmt
+		MemberDTO dto=null;
+		try {
+			pstmt = conn.prepareStatement("select * from tbl_member where memId=?");
+			pstmt.setString(1, id);
+		
+			rs = pstmt.executeQuery();
+			if(rs!=null) {
+				while(rs.next()) {
+					dto = new MemberDTO();
+					dto.setMemId(rs.getString("memId"));
+					dto.setPwd(rs.getString("pwd"));
+					dto.setName(rs.getString("name"));
+					dto.setRole(rs.getInt("role"));
+				}		
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try{rs.close();}catch(Exception e) {e.printStackTrace();}
+			try{pstmt.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		return dto;
+	}
+	
+	
+}
